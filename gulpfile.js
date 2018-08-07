0aef93e1179c94903645706b55ba720d9f092a80
Denis Popov <deniolp@gmail.com>
2018-08-07 16:31:35 +0300

    Настройка релоада

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var gulp = require("gulp");
+var server = require("browser-sync").create();
+
+gulp.task("serve", function() {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/*.html").on("change", server.reload);
+});
