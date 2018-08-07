b4d53f2641da25e59f2fc980959982633e78ed4f
Denis Popov <deniolp@gmail.com>
2018-08-07 22:47:22 +0300

    Добавление цикла, отрисовывающего группу элементов с данными из data

--- source/index.js
+++ source/index.js
@@ -7,6 +7,19 @@ var app = new Vue({
         image: './img/vmSocks-green-onWhite.jpg',
         alt: 'Amazing socks',
         link: 'https://www.yandex.ru/search/?text=socks%20&lr=43&rnd=92501',
-        inventory: 8
+        inventory: 8,
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+          {
+            variantId: 2234,
+            variantColor: 'green'    
+          },
+          {
+            variantId: 2235,
+            variantColor: 'blue'
+          }
+        ],
+        sizes: [44, 45]
     }
 });
