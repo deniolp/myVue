c720ec5046e1c3b7fbfc2019abf63ea5f54ae841
Denis Popov <deniolp@gmail.com>
2018-08-07 18:03:10 +0300

    Внедрение реального проекта

--- source/index.js
+++ source/index.js
@@ -3,6 +3,8 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        product: 'Socks',
+        image: './img/vmSocks-green-onWhite.jpg',
+        alt: 'Amazing socks'
     }
 });
