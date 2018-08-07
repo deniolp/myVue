164bc26e81b974bf7e0f3387e8cd7643173db9a6
Denis Popov <deniolp@gmail.com>
2018-08-07 22:05:21 +0300

    Добавление link в data и рендеринг ссылки в ДОМе

--- source/index.js
+++ source/index.js
@@ -5,6 +5,7 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './img/vmSocks-green-onWhite.jpg',
-        alt: 'Amazing socks'
+        alt: 'Amazing socks',
+        link: 'https://www.yandex.ru/search/?text=socks%20&lr=43&rnd=92501'
     }
 });
